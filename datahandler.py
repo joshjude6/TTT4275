8f5da326713ec9c5b764b5b3eeeba9f0eeaeaa92
ipeglin <philip@eglin.no>
2024-04-03 14:51:57 +0200

    adding type hinting

--- datahandler.py
+++ datahandler.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def get_data(filename='', header=0):
+def get_data(filename='', header=0) -> pd.DataFrame:
     return pd.read_csv(filename, header=header)
 
 def separate_data(data, training_samples=-1) -> tuple[list, list]:
@@ -8,14 +8,11 @@ def separate_data(data, training_samples=-1) -> tuple[list, list]:
     test_data = data[training_samples:]
     return training_data, test_data
 
-def drop_entry_columns(data, column_names):
+def drop_entry_columns(data, column_names) -> pd.DataFrame:
     data.columns = data.columns.str.strip()
     data = data.drop(columns=column_names)
     return data
 
 if __name__ == '__main__':
-    data = get_data('training_data/iris.data')
-    print('using get_data', data)
-
-    data = get_data('training_data/iris.data')
-    print('using get_data', data.shape)
+    data = get_data('data/iris.data')
+    print(f'Fetched data of shape ({data.shape})', data)

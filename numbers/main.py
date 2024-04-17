111f4b8ff75dcaa5671eb7037bb8bbe2d87be82c
ipeglin <philip@eglin.no>
2024-04-17 09:59:18 +0200

    reading test data and label data

--- numbers/main.py
+++ numbers/main.py
@@ -7,25 +7,28 @@ from os import path
 
 SHOW_PLOT = False
 
-def read_bin(path):
+def read_bin(path, reshape=False):
   with open(path, 'rb') as file:
     _, size = struct.unpack(">II", file.read(8))
     nrows, ncols = struct.unpack(">II", file.read(8))
-    data = np.fromfile(file, dtype='uint8').reshape((size, nrows, ncols))
+    data = np.fromfile(file, dtype='uint8')
 
-  return data, (nrows, ncols)
+    if reshape:
+      data = data.reshape((size, nrows, ncols))
+
+  return data
 
 if __name__ == '__main__':
   dirname, filename = path.split(path.abspath(__file__))
-  training_data, _ = read_bin(f'{dirname}/data/train_images.bin')
+  training_data = read_bin(f'{dirname}/data/train_images.bin', reshape=True)
   print('Training data:', training_data.shape)
-  # training_labels, _ = read_bin(f'{dirname}/data/train_labels.bin')
-  # print('Training labels:', training_labels.shape)
+  training_labels = read_bin(f'{dirname}/data/train_labels.bin')
+  print('Training labels:', training_labels.shape)
   
-  # test_data, _ = read_bin(f'{dirname}/data/test_images.bin')
-  # print('Test data:', test_data.shape)
-  # test_labels, _ = read_bin(f'{dirname}/data/test_labels.bin')
-  # print('Test labels:', test_labels.shape)
+  test_data = read_bin(f'{dirname}/data/test_images.bin', reshape=True)
+  print('Test data:', test_data.shape)
+  test_labels = read_bin(f'{dirname}/data/test_labels.bin')
+  print('Test labels:', test_labels.shape)
 
   try:
     os.mkdir(f'{dirname}/tmp')
@@ -35,17 +38,29 @@ if __name__ == '__main__':
   except FileExistsError:
     print('INFO: Directory already exists')
 
+  # Split data into chunks
   samples_per_chunk = 1000
-  number_of_chunks = training_data.shape[0] // samples_per_chunk  # Integer division for number of chunks
-  chunked_data = np.array_split(training_data, number_of_chunks, axis=0)
-  print(f'INFO: Training data split into {number_of_chunks} chunks of size {samples_per_chunk}')
+  number_of_training_chunks = training_data.shape[0] // samples_per_chunk  # Integer division for number of chunks
+  number_of_label_chunks = training_labels.shape[0] // samples_per_chunk
+  chunked_data = np.array_split(training_data, number_of_training_chunks, axis=0)
+  chuncked_labels = np.array_split(training_labels, number_of_label_chunks, axis=0)
+  print(f'INFO: Training data split into {number_of_training_chunks} chunks of size {samples_per_chunk}')
+  print(f'INFO: Training labels split into {number_of_label_chunks} chunks of size {samples_per_chunk}')
+
+  number_of_testing_chunks = test_data.shape[0] // samples_per_chunk
+  number_of_testing_labels = test_labels.shape[0] // samples_per_chunk
+  chunked_test_data = np.array_split(test_data, number_of_testing_chunks, axis=0)
+  chunked_test_labels = np.array_split(test_labels, number_of_testing_labels, axis=0)
+  print(f'INFO: Test data split into {number_of_testing_chunks} chunks of size {samples_per_chunk}')
+  print(f'INFO: Test labels split into {number_of_testing_labels} chunks of size {samples_per_chunk}')
 
   if SHOW_PLOT:
     # Plot first 9 samples from chunk 0
-    for i in range(9):
-      first_samples = chunked_data[0][:9]
-      plt.subplot(330 + 1 + i)
-      plt.imshow(first_samples[i], cmap=plt.get_cmap('gray'))
-      plt.suptitle('First 9 samples from chunk 0')
-      plt.tight_layout()
-    plt.show()
+    for data_set in [chunked_data, chunked_test_data]:
+      for i in range(9):
+        first_samples = data_set[0][:9]
+        plt.subplot(330 + 1 + i)
+        plt.imshow(first_samples[i], cmap=plt.get_cmap('gray'))
+        plt.suptitle('First 9 samples from chunk 0')
+        plt.tight_layout()
+      plt.show()

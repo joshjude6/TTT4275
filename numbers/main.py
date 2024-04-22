f55f9c9cccc7c2103a814278d87308c62be2f7a3
ipeglin <philip@eglin.no>
2024-04-22 09:59:30 +0200

    updating iris script. also making chunking optional in numbers

--- numbers/main.py
+++ numbers/main.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import numbersFunctions
-from keras.datasets import mnist
 import seaborn as sns
-from sklearn.metrics import confusion_matrix
+import time
+
+from keras.datasets import mnist
 from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix
 from os import path
 
-SHOW_PLOTS = True
+SHOW_PLOTS = False
 SAVE_PLOTS = False
+CHUNK_TRAINING_DATA = True
 
 def euclideanDistance(x, y):
     return np.linalg.norm(x - y) #calculates euclidian distance using norm
@@ -76,31 +78,32 @@ Test labels: {test_labels.shape}
 ''')
 
   # Split data into chunks
-  samples_per_chunk = 1000
-  number_of_training_chunks = training_data.shape[0] // samples_per_chunk  # Integer division for number of chunks
-  number_of_label_chunks = training_labels.shape[0] // samples_per_chunk
-  chunked_training_data = np.array(np.array_split(training_data, number_of_training_chunks, axis=0))
-  chunked_training_labels = np.array(np.array_split(training_labels, number_of_label_chunks, axis=0))
-  print(f'''INFO: Training data split into {number_of_training_chunks} chunks of size {chunked_training_data.shape[1]}
-INFO: Training labels split into {number_of_label_chunks} chunks of size {chunked_training_labels.shape[1]}''')
+  if CHUNK_TRAINING_DATA:
+    samples_per_chunk = 1000
+    number_of_training_chunks = training_data.shape[0] // samples_per_chunk  # Integer division for number of chunks
+    number_of_label_chunks = training_labels.shape[0] // samples_per_chunk
+    training_data = np.array(np.array_split(training_data, number_of_training_chunks, axis=0))
+    training_labels = np.array(np.array_split(training_labels, number_of_label_chunks, axis=0))
+    print(f'''INFO: Training data split into {number_of_training_chunks} chunks of size {training_data.shape[1]}
+INFO: Training labels split into {number_of_label_chunks} chunks of size {training_labels.shape[1]}''')
 
-  number_of_testing_chunks = test_data.shape[0] // samples_per_chunk
-  number_of_testing_labels = test_labels.shape[0] // samples_per_chunk
-  chunked_test_data = np.array(np.array_split(test_data, number_of_testing_chunks, axis=0))
-  chunked_test_labels = np.array(np.array_split(test_labels, number_of_testing_labels, axis=0))
-  print(f'''INFO: Test data split into {number_of_testing_chunks} chunks of size {chunked_test_data.shape[1]}
-INFO: Test labels split into {number_of_testing_labels} chunks of size {chunked_test_labels.shape[1]}
+    number_of_testing_chunks = test_data.shape[0] // samples_per_chunk
+    number_of_testing_labels = test_labels.shape[0] // samples_per_chunk
+    test_data = np.array(np.array_split(test_data, number_of_testing_chunks, axis=0))
+    test_labels = np.array(np.array_split(test_labels, number_of_testing_labels, axis=0))
+    print(f'''INFO: Test data split into {number_of_testing_chunks} chunks of size {test_data.shape[1]}
+INFO: Test labels split into {number_of_testing_labels} chunks of size {test_labels.shape[1]}
 ''')
   
-  print(f'''Training data: {chunked_training_data.shape}
-Training labels: {chunked_training_labels.shape}
-Test data: {chunked_test_data.shape}
-Test labels: {chunked_test_labels.shape}
+  print(f'''Training data: {training_data.shape}
+Training labels: {training_labels.shape}
+Test data: {test_data.shape}
+Test labels: {test_labels.shape}
 ''')
 
   if SHOW_PLOTS:
     # Plot first 9 samples from chunk 0
-    for data_set in [chunked_training_data, chunked_test_data]:
+    for data_set in [training_data, test_data]:
       for i in range(9):
         first_samples = data_set[0][:9]
         plt.subplot(330 + 1 + i)
@@ -110,27 +113,35 @@ Test labels: {chunked_test_labels.shape}
       plt.show()
 
   # Normalize data
-  chunked_training_data = chunked_training_data / 255
-  chunked_test_data = chunked_test_data / 255
+  training_data = training_data / 255
+  test_data = test_data / 255
 
   # One hot encode labels
   one_hot_encode = lambda labels: np.eye(max(labels + 1))[labels]
-  encoded_training_labels = np.array([one_hot_encode(label_chunk) for label_chunk in chunked_training_labels])
-  encoded_test_labels = np.array([one_hot_encode(label_chunk) for label_chunk in chunked_test_labels])
+  encoded_training_labels = np.array([one_hot_encode(label_chunk) for label_chunk in training_labels])
+  encoded_test_labels = np.array([one_hot_encode(label_chunk) for label_chunk in test_labels])
   print(f'''Encoded Training Labels: {encoded_training_labels.shape}
 Encoded Test Labels {encoded_test_labels.shape}
 ''')
   
   # Evaluate nearest neighbor classifier
-  for i in range(9):
-    accuracy, predicted_labels = evaluateNearestNeighbor(chunked_training_data[i], chunked_training_labels[i], chunked_test_data[i], chunked_test_labels[i])
-    print("Accuracy:", accuracy)
+  print('Evaluating for each test data chunk...')
+  start_time = time.time()
+  for i in range(test_data.shape[0]):
+    chunk_timer_start = time.time()
+    accuracy, predicted_labels = evaluateNearestNeighbor(training_data[i], training_labels[i], test_data[i], test_labels[i])
+    print(f'Chunk #{i + 1} Accuracy: {accuracy} | Time elapsed: {time.time() - chunk_timer_start:.2f}s')
 
     # Plot confusion matrix
-    cm = confusion_matrix(chunked_test_labels[i], predicted_labels)
+    cm = confusion_matrix(test_labels[i], predicted_labels)
     plt.figure(figsize=(10, 8))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
+
+  print('Finished!')
+  print(f"Total time elapsed: {time.time() - start_time:.2f}s")
+
+  if SHOW_PLOTS:
     plt.show()

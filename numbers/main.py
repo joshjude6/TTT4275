ea64f7d388f3a592ff574d27a17cd69516cffec4
joshjude6 <115927427+joshjude6@users.noreply.github.com>
2024-04-30 11:19:26 +0200

    fixed missing line in clustering

--- numbers/main.py
+++ numbers/main.py
@@ -106,6 +106,7 @@ def KMeansClustering(trainingData, trainingLabels, nClusters):
     clusterTemplates = {} 
     for classLabel in np.unique(trainingLabels): 
         classData = trainingData[trainingLabels == classLabel].reshape(-1, (28*28)) 
+        kmeans.fit(classData)
         clusterTemplates[classLabel] = kmeans.cluster_centers_ 
     return clusterTemplates
 
